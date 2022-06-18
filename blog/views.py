4283e53c4c9ba22fcbad829f3fd087b6c3b67850
Gabriel Rodrigues <euumgabriel@gmail.com>
2022-06-17 23:28:00 -0300

    Feature to share posts by email added.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.mail import send_mail
 
 from .models import Post
+from .forms import EmailPostForm
 
 
 def post_list(request):
@@ -32,3 +34,28 @@ def post_detail(request, year, month, day, post):
     )
 
     return render(request, "blog/post/detail.html", {"post": post})
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status="published")
+
+    sent = False
+
+    if request.method == "POST":
+        form = EmailPostForm(request.POST)
+
+        if form.is_valid():
+            cleaned_data = form.cleaned_data
+
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cleaned_data['name']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url}\n\n{cleaned_data['name']}'s comments: {cleaned_data['comments']}"
+            send_mail(subject, message, "admin@myblog.com", [cleaned_data["to"]])
+
+            sent = True
+    else:
+        form = EmailPostForm()
+
+    return render(
+        request, "blog/post/share.html", {"post": post, "form": form, "sent": sent}
+    )
--- config/settings.py
+++ config/settings.py
@@ -119,3 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
+
+# Email config
+# EMAIL_HOST = ""
+# EMAIL_PORT = ""
+# EMAIL_HOST_USER = ""
+# EMAIL_HOST_PASSWORD = ""
+# EMAIL_USE_TLS = ""
+# EMAIL_USE_SSL = ""
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
